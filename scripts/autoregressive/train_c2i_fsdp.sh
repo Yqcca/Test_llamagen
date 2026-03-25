b8c8b8e0615cc6ba8dcc1491255d313fe30c2e2f
yqcca <a1024862884@gmail.com>
2026-03-25 17:06:30 +0800

    .

--- scripts/autoregressive/train_c2i_fsdp.sh
+++ scripts/autoregressive/train_c2i_fsdp.sh
@@ -6,7 +6,7 @@ set -x
 # --master_addr=$master_addr --master_port=$master_port \
 # autoregressive/train/train_c2i_fsdp.py "$@"
 
-CUDA_VISIBLE_DEVICES=0 torchrun \
+CUDA_VISIBLE_DEVICES=7 torchrun \
 --nnodes=1 --nproc_per_node=1 --node_rank=0 \
 --master_addr=localhost --master_port=12345 \
 autoregressive/train/train_c2i_fsdp.py "$@"
